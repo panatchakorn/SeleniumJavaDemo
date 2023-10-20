2c2867690f791ff97764d545d2bb7aa7a8ba0fc8
Panatchakorn <panatchakorn@gmail.com>
2023-10-21 08:58:28 +1300

    disable skipped test

--- src/test/java/testcase/StampResultsTest.java
+++ src/test/java/testcase/StampResultsTest.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
 import org.testng.Assert;
+import org.testng.ITestResult;
 import org.testng.SkipException;
 import org.testng.annotations.Test;
 
@@ -20,11 +21,10 @@ public class StampResultsTest extends BaseTest {
         nodeSetup.log(Status.INFO,"Check if true is true");
         Assert.assertEquals(true,true,"This test should passed");
     }
-    @Test
+    @Test(enabled = false)
     public void skippedTest(){
         ExtentTest nodeSetup = getTest().createNode("Make the test Skipped");
-        nodeSetup.log(Status.INFO,"Check for terst skipped");
-        throw new SkipException("This test should skipped");
+        nodeSetup.log(Status.INFO,"Check for test skipped");
     }
     @Test
     public void failedTest(){
