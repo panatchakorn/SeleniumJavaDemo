ab859392e536f4a7b120fc8be1b008689e0c2820
Panatchakorn <panatchakorn@gmail.com>
2023-10-21 22:29:12 +1300

    fix surefire dump Boot Manifest-JAR contains absolute paths error

--- src/test/java/testcase/StampResultsTest.java
+++ src/test/java/testcase/StampResultsTest.java
@@ -3,28 +3,27 @@ package testcase;
 import base.BaseTest;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
-import org.openqa.selenium.*;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
 import org.testng.Assert;
 import org.testng.ITestResult;
+import org.testng.Reporter;
 import org.testng.SkipException;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
 
-public class StampResultsTest extends BaseTest {
+public class StampResultsTest extends BaseTest{
     @Test
     public void passedTest(){
         ExtentTest nodeSetup = getTest().createNode("Make the test Passed");
         nodeSetup.log(Status.INFO,"Check if true is true");
         Assert.assertEquals(true,true,"This test should passed");
     }
-    @Test(enabled = false)
+    @Test
     public void skippedTest(){
         ExtentTest nodeSetup = getTest().createNode("Make the test Skipped");
         nodeSetup.log(Status.INFO,"Check for test skipped");
+        ITestResult result = Reporter.getCurrentTestResult();
+        result.setStatus(ITestResult.SKIP);
+        result.setThrowable(new SkipException("This test should skipped"));
     }
     @Test
     public void failedTest(){
