4cb3fe6da8f3459cc1c6843456068907b4dc3218
Panatchakorn <panatchakorn@gmail.com>
2023-10-26 22:03:18 +1300

    Add more tests

---
+++ src/test/java/testcase/BundleDTest.java
@@ -0,0 +1,33 @@
+package testcase;
+
+import base.BaseTest;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+import org.testng.SkipException;
+import org.testng.annotations.Test;
+
+public class BundleDTest extends BaseTest {
+    @Test
+    public void bundleDPassedTest(){
+        ExtentTest nodeSetup = getTest().createNode("Make the test Passed");
+        nodeSetup.log(Status.INFO,"Check if true is true");
+        Assert.assertEquals(true,true,"This test should passed");
+    }
+    @Test
+    public void bundleDSkippedTest(){
+        ExtentTest nodeSetup = getTest().createNode("Make the test Skipped");
+        nodeSetup.log(Status.INFO,"Check for test skipped");
+        ITestResult result = Reporter.getCurrentTestResult();
+        result.setStatus(ITestResult.SKIP);
+        result.setThrowable(new SkipException("This test should skipped"));
+    }
+    @Test
+    public void bundleDFailedTest(){
+        ExtentTest nodeSetup = getTest().createNode("Make the test Failed");
+        nodeSetup.log(Status.INFO,"Check for test failed");
+        Assert.assertEquals(false,true,"This test should failed");
+    }
+}
