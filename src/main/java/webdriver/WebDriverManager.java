a16b2c4b213a4ff3a450d5ddb0516bb7443a2e49
Panatchakorn <panatchakorn@gmail.com>
2024-06-05 21:39:36 +1200

    check maven

--- src/main/java/webdriver/WebDriverManager.java
+++ src/main/java/webdriver/WebDriverManager.java
@@ -1,6 +1,9 @@
 package webdriver;
 
 import org.openqa.selenium.WebDriver;
+import utils.ConfigReader;
+
+import java.net.MalformedURLException;
 
 public class WebDriverManager {
     private WebDriverManager() {
@@ -8,12 +11,24 @@ public class WebDriverManager {
     }
 
     private static final ThreadLocal<WebDriver> webDriverThreadLocal = new ThreadLocal<>();
+    static final ConfigReader configReader = new ConfigReader();
 
-    public static WebDriver getDriver(BrowserType browserType) {
+    public static WebDriver getDriver(BrowserType browserType) throws MalformedURLException {
         WebDriver driver = webDriverThreadLocal.get();
 
         if (driver == null) {
-            driver = DriverFactory.createDriver(browserType);
+            if (configReader.getConfigKey("runOnGrid").equalsIgnoreCase("true")) {
+                // RemoteWebDriver
+                try {
+                    driver = DriverFactory.createRemoteWebDriver(browserType);
+                } catch (MalformedURLException e) {
+                    throw new MalformedURLException("Invalid Selenium grid url: " + e);
+                }
+            } else {
+                // Local WebDriver
+                driver = DriverFactory.createDriver(browserType);
+            }
+
             webDriverThreadLocal.set(driver);
         }
 
