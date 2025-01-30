38ba8a5e73f5f7c86aee2a99a36836bff878ddd8
panatchakorn <panatchakorn@gmail.com>
2025-01-31 10:38:18 +1300

    5000 tests each

--- src/test/java/testcase/LogTest.java
+++ src/test/java/testcase/LogTest.java
@@ -23,8 +23,8 @@ public class LogTest extends BaseTest {
     }
     @DataProvider(name = "numberProvider")
     public Object[][] numberProvider() {
-        Object[][] data = new Object[501][1];
-        for (int i = 0; i <= 500; i++) {
+        Object[][] data = new Object[5001][1];
+        for (int i = 0; i <= 5000; i++) {
             data[i][0] = i;
         }
         return data;
