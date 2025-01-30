66036f5c6cc3afd3a0ecc3f3ddaf6b71799cd603
Panatchakorn <panatchakorn@gmail.com>
2025-01-31 01:29:42 +1300

    assert log test

--- src/test/java/testcase/LogTest.java
+++ src/test/java/testcase/LogTest.java
@@ -4,6 +4,8 @@ import base.BaseTest;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import utils.ConfigReader;
@@ -11,6 +13,14 @@ import utils.ConfigReader;
 public class LogTest extends BaseTest {
     private static final Logger LOGGER = LogManager.getLogger(BaseTest.class.getName());
 
+   @BeforeMethod
+    public void beforeMethod() {
+        LOGGER.info("Before method");
+    }
+    @AfterMethod
+    public void afterMethod() {
+        LOGGER.info("After method");
+    }
     @DataProvider(name = "numberProvider")
     public Object[][] numberProvider() {
         Object[][] data = new Object[10001][1];
