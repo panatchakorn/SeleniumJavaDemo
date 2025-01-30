359ab2989ba51512b77d99e84341cbb88633057d
Panatchakorn <panatchakorn@gmail.com>
2025-01-31 01:05:10 +1300

    assert log test

--- src/test/java/testcase/LogTest.java
+++ src/test/java/testcase/LogTest.java
@@ -1,15 +1,20 @@
 package testcase;
 
 import base.BaseTest;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import utils.ConfigReader;
 
 public class LogTest extends BaseTest {
+    private static final Logger LOGGER = LogManager.getLogger(BaseTest.class.getName());
 
     @DataProvider(name = "numberProvider")
     public Object[][] numberProvider() {
-        Object[][] data = new Object[10001][1];
-        for (int i = 0; i <= 10000; i++) {
+        Object[][] data = new Object[11][1];
+        for (int i = 0; i <= 10; i++) {
             data[i][0] = i;
         }
         return data;
@@ -17,11 +22,16 @@ public class LogTest extends BaseTest {
 
     @Test(dataProvider = "numberProvider")
     public void logTest1(int number) {
-        System.out.println("Test1 log no " + number);
+        String lineNo = "Line no " + number;
+        LOGGER.error("Test1 log {}",  lineNo);
+        Assert.assertEquals("Line no " + number, lineNo);
+
     }
 
     @Test(dataProvider = "numberProvider")
     public void logTest2(int number) {
-        System.out.println("Test2 log no " + number);
+        String lineNo = "Line no " + number;
+        LOGGER.error("Test2 log {}",  lineNo);
+        Assert.assertEquals("Line no " + number, lineNo);
     }
 }
